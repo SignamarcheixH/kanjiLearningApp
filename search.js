80334d4a1d9dba1694ffacd7fe5885a4ccd30d95
SignamarcheixH <signamarcheix.h@gmail.com>
2019-05-03 20:47:08 +0200

    small update on search page + fix some kanji

--- search.js
+++ search.js
@@ -6,17 +6,19 @@ function initList(){
 		let node = document.createElement("LI");
 		node.dataset.kanji=kanji.drawing;
 		node.innerHTML = kanji.drawing;
+		if(kanji.drawing.length >= 3) { node.style.fontSize = '35px'; }
 		node.classList.add("kanji-thumb");
 		kanjiList.appendChild(node);
 		node.addEventListener("click", (event) => {
 			showInfo(node.dataset.kanji, event);		
 		})
 	}
+	let totalPlaceholder = document.getElementsByClassName("total-placeholder")[0];
+	totalPlaceholder.innerHTML = allKanji.length;
 }
 
 function showInfo(refKanji, event) {
 	let allKanji = JSON.parse(data);
-	console.log(event.clientX);
 	let infoCard = document.getElementsByClassName("info-card")[0];
 	if(event.clientX > 611) {
 		infoCard.style.left = '10%';
@@ -25,8 +27,13 @@ function showInfo(refKanji, event) {
 	}
 	for (kanji of allKanji) {
 		if(kanji.drawing == refKanji) {
-			infoCard.style.opacity = 1;
+			infoCard.classList.add("is-active");
 			infoCard.getElementsByClassName("card-placeholder")[0].innerHTML = kanji.drawing;
+			if (kanji.drawing.length >= 3 ) { 
+				infoCard.getElementsByClassName("card-placeholder")[0].style.fontSize = '100px';
+				infoCard.getElementsByClassName("card-placeholder")[0].style.paddingTop = '30px';
+				infoCard.getElementsByClassName("card-placeholder")[0].style.paddingBottom = '30px';
+			}
 			infoCard.getElementsByClassName("card-meaning")[0].innerHTML = kanji.meaning;
 			infoCard.getElementsByClassName("card-kunyomi")[0].innerHTML = kanji.kunyomi;
 			infoCard.getElementsByClassName("card-onyomi")[0].innerHTML = kanji.onyomi;
@@ -39,6 +46,6 @@ document.addEventListener("DOMContentLoaded", () => {
 	let closeIcon = document.getElementsByClassName("icon-close")[0];
 	let infoCard = document.getElementsByClassName("info-card")[0];
 	closeIcon.addEventListener("click", () => {
-		infoCard.style.opacity = 0;
+		infoCard.classList.remove("is-active");
 	})
 });
